3038e25f12453b87489744a36c3d0d7a40b0300d
Elena <hardevich@gmail.com>
2023-04-11 20:58:53 +0400

    Example003 add random sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,11 @@
-﻿int numberA = 30;
+﻿int numberA = 20;
 int numberB = 5;
 int res = numberA + numberB;
 Console.WriteLine(res);
+
+int numberC = new Random().Next(1, 10);
+Console.WriteLine(numberC);
+int numberD = new Random().Next(1, 10);
+Console.WriteLine(numberD);
+int result = numberC + numberD;
+Console.WriteLine(result);
